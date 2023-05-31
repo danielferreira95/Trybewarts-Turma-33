d193cfd101c3c0379ab4e83aa837267a69706294
DanielFerreira <daniel18mtm@gmail.com>
2023-05-31 19:29:55 -0300

    Requisito 18 feito

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const email = document.getElementById('email');
 const botao = document.querySelector('.btn-submit');
 const senha = document.getElementById('senha');
+const checkboxAgreement = document.getElementById('agreement');
+const btnEnviar = document.querySelector('#submit-btn');
+const spanSubmit = document.querySelector('#span-submit');
 
 const valid = () => {
   if (email.value === 'tryber@teste.com' || senha.value === '123456') {
@@ -10,4 +13,20 @@ const valid = () => {
   }
 };
 
+const verificaAgreement = () => {
+  console.log(document.querySelector('#agreement:checked'));
+  if(document.querySelector('#agreement:checked')){
+  btnEnviar.removeAttribute('disabled');
+  spanSubmit.style.color = 'green';
+  }else{
+    btnEnviar.setAttribute('disabled', 'disabled');
+    spanSubmit.style.color = 'red';
+  }
+};
+
 botao.addEventListener('click', valid);
+checkboxAgreement.addEventListener('click', verificaAgreement);
+
+
+
+
